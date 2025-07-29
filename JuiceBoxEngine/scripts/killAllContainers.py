b06c4c570758b7a4c2b443a5bebcc865685cd2de
Edgar Sabido <edgar.sabido17@gmail.com>
2025-07-29 00:15:16 -0600

    TUI and JBEngine: edited

--- JuiceBoxEngine/scripts/killAllContainers.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import time, docker
-
-if __name__ == "__main__":
-    # Para y destruye todos los contenedores existentes:
-    client = docker.from_env()
-    for container in client.containers.list(all=True):
-        container.stop()
-        container.remove()
